cb0548ecf3ef33881fcd921f732edaf47b8fc1ea
mf <mf@localhost.localdomain>
2020-06-04 17:41:45 +0700

    Update

--- vm/variables.tf
+++ vm/variables.tf
@@ -1,6 +1,16 @@
-variable "vm_name" {}
-variable "machine_type" {}
-variable "zone" {}
+variable "vm_name" {
+  description = "Name for instance"
+}
+
+variable "machine_type" {
+  description = "Machine type for VM. See: https://cloud.google.com/compute/docs/machine-types"
+}
+
+variable "zone" {
+  description = "Zone for region. See: https://cloud.google.com/compute/docs/regions-zones"
+}
+
 variable "image" {
-    default = "debian-cloud/debian-9"
+  description = "Image that VM will running on. See: https://cloud.google.com/compute/docs/images"
+  default     = "debian-cloud/debian-9"
 }
