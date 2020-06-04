d1fd3192d2faad2c9b3e16b51e196f7954decd61
mf <mf@localhost.localdomain>
2020-06-04 14:49:32 +0700

    Update

--- vm-instance/main.tf
+++ vm-instance/main.tf
@@ -2,4 +2,17 @@ resource "google_compute_instance" "default" {
     name = var.vm_name
     machine_type = var.machine_type
     zone = var.zone
+
+      boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
 }
